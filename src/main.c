3e846590393a2fedf9e741d474024e9ba4789273
Birdabo404 <birdabo420@gmail.com>
2025-02-27 19:11:54 +0800

    REPL

--- src/main.c
+++ src/main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int main(){
 
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  input[strcspn(input, "\n")] = 0;
-  printf("%s: command not found\n", input);
+  using_history();
+  char *input; 
+  while ((input = readline ("mussel-0.1$ ")) != NULL){
+    printf("%s: command not found!\n", input); 
+    add_history(input);
+    free(input);
+  }
+  clear_history();
   return 0;
 }
